7af063586e630bd4e6496a76597f6354621f7163
1895271 <1895271@E21015.CEGEPGIM.LCL>
2019-02-17 09:31:07 -0500

    Created class Drive with attributes, constructor and the getter/setter methods and pulled changes

---
+++ src/jhakasstaxi/Drive.java
@@ -0,0 +1,59 @@
+package jhakasstaxi;
+
+import java.util.Date;
+
+public class Drive {
+    Date dateOfDrive;
+    int duration;
+    Double distance;
+    String origin;
+    String destination;
+    
+    public Drive(Date dateOfDrive, int duration, Double distance, String origin, String destination) {
+        this.dateOfDrive = dateOfDrive;
+        this.duration = duration;
+        this.distance = distance;
+        this.origin = origin;
+        this.destination = destination;
+    }
+    
+    public void setDateOfDrive(Date dateOfDrive) {
+        this.dateOfDrive = dateOfDrive;
+    }
+    
+    public Date getDateOfDrive() {
+        return dateOfDrive;
+    }
+    
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    public int getDuration() {
+        return duration;
+    }
+    
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+    
+    public Double getDistance() {
+        return distance;
+    }
+    
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+    
+    public String getOrigin() {
+        return origin;
+    }
+    
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+    
+    public String getDestination() {
+        return destination;
+    }
+}
--- src/jhakasstaxi/Driver.java
+++ src/jhakasstaxi/Driver.java
@@ -1,3 +1,5 @@
+package jhakasstaxi;
+
 
 import java.util.Date;
 
