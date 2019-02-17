1a91f1f69715bc8a20e2bc3e9b697023ae7c702b
1895127 <1895127@E21029.CEGEPGIM.LCL>
2019-02-17 11:35:29 -0500

    changes made

--- src/jhakasstaxi/Rating.java
+++ src/jhakasstaxi/Rating.java
@@ -37,4 +37,7 @@ public class Rating {
     public String getComment() {
         return comment;
     }
-}
+    public void displayTaxi() {
+        System.out.println("Driver's Rating: " + rate + " Comment " + comment );
+    }
+    }
